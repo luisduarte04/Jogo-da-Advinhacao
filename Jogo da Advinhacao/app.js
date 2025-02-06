b6ffdf598e21ab468901a947493028a9eee7152c
Luis Guilherme <lgduarte.santos2@hotmail.com>
2025-02-06 20:51:38 -0300

    Somando número de tentativas e verificando resposta do usuário

--- Jogo da Advinhacao/app.js
+++ Jogo da Advinhacao/app.js
@@ -1,4 +1,5 @@
 let num = gerarnumeroAleat()
+let qntd = 1
 
 
 function telaInicial(seletor, mensagem) {
@@ -12,5 +13,29 @@ telaInicial("p", "Escolha um número de 1 a 10:")
 
 function gerarnumeroAleat() {
     return parseInt(Math.random() * 11)
+}
+
+
+function verificarChute() {
+    let chute = document.querySelector("input").value
+    let palavraTentativa = qntd == 1 ? "tentativa" : "tentativas"
+    if (chute == num) {
+        limparinput()
+        telaInicial("h1", "Parabéns!")
+        telaInicial("p", `Você acertou em ${qntd} ${palavraTentativa}`)
+    } else {
+        qntd += 1
+        limparinput()
+        if (chute > num) {
+            telaInicial("p", "O número é menor...")
+        } else {
+            telaInicial("p", "O número é maior...")
+        }
+    }
+
+}
+
+function limparinput() {
+    document.querySelector("input").value = ""
 }
 
